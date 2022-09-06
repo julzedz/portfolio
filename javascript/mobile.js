6099f3ab6f9c5064fe8e0535f73d07be7830e1d2
Edozie <edoziechukwuma@gmail.com>
2022-09-06 17:51:58 +0100

    correct linter errors

--- javascript/mobile.js
+++ javascript/mobile.js
@@ -1,19 +1,17 @@
 const hamburger = document.querySelector('#hamburger-toggle');
 const nav = document.querySelector('nav');
-const navIcon = document.querySelectorAll('.nav-icon')
-const close = document.querySelector('.close')
-const menuItem = document.querySelector('menu-item')
+const navIcon = document.querySelectorAll('.nav-icon');
+const close = document.querySelector('.close');
+// const menuItem = document.querySelector('menu-item')
 
-hamburger.addEventListener('click',(e) => {
-  nav.classList.toggle('open')
+hamburger.addEventListener('click', (e) => {
+  nav.classList.toggle('open');
   e.preventDefault();
-  close.classList.toggle('visible')
-  navIcon.forEach(icon => {
+  close.classList.toggle('visible');
+  navIcon.forEach((icon) => {
     icon.classList.toggle('hidden');
-  })
-}
-)
-
+  });
+});
 
 // menuItem.addEventListener('click',(e) => {
 //   e.preventDefault()
