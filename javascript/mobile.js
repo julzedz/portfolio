9a2fd35b5a36fbdf11d62d15427a78dddb93fefa
Edozie <edoziechukwuma@gmail.com>
2022-09-06 17:14:06 +0100

    modify mobile menu

--- javascript/mobile.js
+++ javascript/mobile.js
@@ -2,7 +2,7 @@ const hamburger = document.querySelector('#hamburger-toggle');
 const nav = document.querySelector('nav');
 const navIcon = document.querySelectorAll('.nav-icon')
 const close = document.querySelector('.close')
-
+const menuItem = document.querySelector('menu-item')
 
 hamburger.addEventListener('click',(e) => {
   nav.classList.toggle('open')
@@ -10,7 +10,12 @@ hamburger.addEventListener('click',(e) => {
   close.classList.toggle('visible')
   navIcon.forEach(icon => {
     icon.classList.toggle('hidden');
-
   })
 }
 )
+
+
+// menuItem.addEventListener('click',(e) => {
+//   e.preventDefault()
+//   nav.classList.remove('open')
+// })
