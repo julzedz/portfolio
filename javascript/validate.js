339ab3370d60e7aaf0ff9c73593852a849996080
Edozie <edoziechukwuma@gmail.com>
2022-09-09 14:01:57 +0100

    Update validate.js

--- javascript/validate.js
+++ javascript/validate.js
@@ -5,12 +5,3 @@ const emailInput = email.value;
 const Emailregex = /^[a-z0-9]+@[a-z0-9-]+\.[a-z0-9-.]+$/;
 const errorMsg = 'Email should only contain lowercase letters.';
 
-formElement.addEventListener('submit', (e) => {
-  if (!emailInput.match(Emailregex)) {
-    error.innerHTML = errorMsg;
-    error.style.display = 'block';
-    e.preventDefault();
-    return false;
-  }
-  return true;
-});
