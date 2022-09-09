cbf3ddecfdc598d6e1c278242fae574b32aaece3
Edozie <edoziechukwuma@gmail.com>
2022-09-09 22:35:31 +0100

    Update validate.js

--- javascript/validate.js
+++ javascript/validate.js
@@ -1,6 +1,6 @@
 const formElement = document.querySelector('#form-element');
 const email = document.querySelector('#emaile');
-const error = document.querySelector('.error');
+const error = document.querySelector('.error'
 const errorMsg = 'Email should only contain lowercase letters.';
 const Emailregex = /^[a-z0-9]+@[a-z0-9-]+\.[a-z0-9-.]+$/;
 
