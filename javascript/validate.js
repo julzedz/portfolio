bd9c4dad34f95671e6cb8be4f61fe02be0929f9b
Edozie <edoziechukwuma@gmail.com>
2022-09-09 22:32:42 +0100

    Summary

--- javascript/validate.js
+++ javascript/validate.js
@@ -4,10 +4,18 @@ const error = document.querySelector('.error');
 const errorMsg = 'Email should only contain lowercase letters.';
 const Emailregex = /^[a-z0-9]+@[a-z0-9-]+\.[a-z0-9-.]+$/;
 
+const formInfo = {
+  fullName: document.querySelector('.full.name').value,
+  emailAddress: document.querySelector('#email').value,
+  textArea: document.querySelector('#text-area').value
+}
+
 formElement.addEventListener('submit', (e) => {
   const emailInput = email.value;
   if (emailInput.match(Emailregex)) {
     error.style.display = 'none';
+    localStorage.setItem('data', JSON.stringify(formInfo));
+
     return true;
   }
   e.preventDefault();
@@ -15,3 +23,7 @@ formElement.addEventListener('submit', (e) => {
   error.innerHTML = errorMsg;
   return false;
 });
+
+let getFormInfo = localStorage.getItem('data');
+getFormInfo = JSON.parse(getFormInfo);
+console.log(getFormInfo)
