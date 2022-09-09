f88c8e0b8822cbf40ae650ddb950c4a3d9712b73
Edozie <edoziechukwuma@gmail.com>
2022-09-09 14:33:44 +0100

    Correct linters errors.

--- javascript/validate.js
+++ javascript/validate.js
@@ -6,11 +6,11 @@ const Emailregex = /^[a-z0-9]+@[a-z0-9-]+\.[a-z0-9-.]+$/;
 const errorMsg = 'Email should only contain lowercase letters.';
 
 formElement.addEventListener('submit', (e) => {
-    if (!emailInput.match(Emailregex)){
-      error.innerHTML = errorMsg;
-      error.style.display = 'block';
-      e.preventDefault();
-      return false;
-    }
-    return true;
-  });
+  if (!emailInput.match(Emailregex)) {
+    error.innerHTML = errorMsg;
+    error.style.display = 'block';
+    e.preventDefault();
+    return false;
+  }
+  return true;
+});
