4a9379332b28656ab268e18f486203bb2836618a
Edozie <edoziechukwuma@gmail.com>
2022-10-08 18:55:58 -0600

    Add a project

--- javascript/modal.js
+++ javascript/modal.js
@@ -1,12 +1,12 @@
 const cardData = [
   {
-    name: 'Multi-Post Stories Gain + Glory',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-    image: './pics/SnapshootDesktopPortfolio.svg',
-    imgMobile: './pics/Snapshoot-Portfolio.svg',
-    linkLive: 'https://julzedz.github.io/',
-    linkSource: 'https://julzedz.github.io/',
+    name: 'Awesome Books',
+    description: 'This application lets you compile a dynamic collection of books using JavaScript. It is a single page app. You enter your book title & author in the Add Book section, and it appears on the List section.',
+    technologies: ['HTML', 'CSS', 'Vanilla JavaScript'],
+    image: './pics/awesomebooks1.png',
+    imgMobile: './pics/awesomebooks1.png',
+    linkLive: 'https://julzedz.github.io/Awesome-Books/',
+    linkSource: 'https://github.com/julzedz/Awesome-Books',
   },
   {
     name: 'Multi-Post Stories Gain + Glory',
