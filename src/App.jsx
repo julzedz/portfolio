4389c8963c6223695dcad1ff9b5bdccdbea3a4bb
julzedz <edoziechukwuma@gmail.com>
2025-04-27 22:02:12 +0100

    Initialize project structure with main App component and routing setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import { Home, About, Projects, Contact } from "./pages";
+
+const App = () => {
+  return (
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import Projects from './Projects'
+
+export { Home, About, Contact, Projects };
