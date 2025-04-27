f2bb286ffacd08bb15546169158331453e30c95c
julzedz <edoziechukwuma@gmail.com>
2025-04-27 21:59:56 +0100

    Update project dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  build: {
-    minify: 'terser',
-  },
-});
+  plugins: [react()],
+})
