545737fa7e192dce1d679b04c6e06aca40fa0edf
julzedz <edoziechukwuma@gmail.com>
2025-04-29 10:53:59 +0100

    Refactor Home component to integrate react-three for 3D rendering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { React, Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import Loader from "../components/Loader";
+import Island from "../models/Island";
+import Sky from "../models/Sky";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
 
-export default Home
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, rotation];
+  };
+
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
+
+  return (
+    <section className="w-full h-screen relative">
+      {/* <div className='top-28 left-0 right-0 z-10 flex items-center justify-center'>
+
+      </div> */}
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
+          <Sky />
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+          />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
+};
+
+export default Home;
