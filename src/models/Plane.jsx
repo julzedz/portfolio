ae281c8eda0222acbfa1a2ef31dd933db4832c3a
julzedz <edoziechukwuma@gmail.com>
2025-05-03 15:31:44 +0100

    Refactor Plane component for consistent formatting and prop integration

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
 
-import planeScene from '../assets/3d/plane.glb';
-import { useGLTF } from '@react-three/drei';
+import planeScene from "../assets/3d/plane.glb";
+import { useAnimations, useGLTF } from "@react-three/drei";
 
-const Plane = () => {
-  const { scene, animations } = useGLTF(planeScene)
+const Plane = ({ isRotating, ...props }) => {
+  const ref = useRef();
+  const { scene, animations } = useGLTF(planeScene);
+  const { actions } = useAnimations(animations, ref);
+
+  useEffect(() => {
+  if(isRotating) {
+      actions['Take 001'].play();
+    } else {
+      actions['Take 001'].stop();
+    }
+  }, [actions, isRotating])
+
+
   return (
-    <mesh>
+    <mesh {...props} ref={ref}>
       <primitive object={scene} />
     </mesh>
-  )
-}
+  );
+};
 
-export default Plane
+export default Plane;
