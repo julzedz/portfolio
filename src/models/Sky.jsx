98f941c5f52317a1f1d7d8f50b2668a3095e9125
julzedz <edoziechukwuma@gmail.com>
2025-05-03 15:33:00 +0100

    Refactor Sky component to improve prop handling and formatting

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,15 +1,23 @@
+import { useGLTF } from "@react-three/drei";
+import React, { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import skyScene from "../assets/3d/sky.glb";
 
-import { useGLTF } from '@react-three/drei'
-import React from 'react'
-import skyScene from '../assets/3d/sky.glb'
+const Sky = ({ isRotating }) => {
+  const sky = useGLTF(skyScene);
+  const skyRef = useRef();
 
-const Sky = () => {
-  const sky = useGLTF(skyScene)
+  useFrame((_, delta) => {
+    if(isRotating) {
+      skyRef.current.rotation.y += 0.15 * delta
+    }
+  })
+
   return (
-    <mesh>
+    <mesh ref={skyRef}>
       <primitive object={sky.scene} />
     </mesh>
-  )
-}
+  );
+};
 
-export default Sky
+export default Sky;
